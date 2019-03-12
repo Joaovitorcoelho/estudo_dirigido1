d42f4cbae7f2b6f083a9682e21fcbb55a15f4053
Joaovitorcoelho <48135760+Joaovitorcoelho@users.noreply.github.com>
2019-03-12 10:33:55 -0300

    Create exercicio3.php

---
+++ exercicio3.php
@@ -0,0 +1,10 @@
+<?php
+
+print "digite quanto é o seu salário:";
+$salario=(float)fgets(STDIN);
+print "digite qual a porcentagem de aumento:";
+$aumento=(float)fgets(STDIN);
+$maisgrana= ($salario*($aumento/100));
+$aumentoo=$maisgrana+$salario;
+print "Você receberá um aumento de $maisgrana";
+print "Seu salário será de $aumentoo";
