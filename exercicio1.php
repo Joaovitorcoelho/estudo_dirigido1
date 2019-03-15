c089bee8d5cf098f603efbfe552829b1d89183b9
Joaovitorcoelho <48135760+Joaovitorcoelho@users.noreply.github.com>
2019-03-15 16:13:32 -0300

    Delete exercicio1.php

--- exercicio1.php
+++
@@ -1,5 +0,0 @@
-<?php
-print "digite qual o tamanho em metros vai ser convertido para milimetros";
-$metros = (int) fgets(STDIN);
-$metross = floor($metros*1000);
-print "você tem $metross milimetros";
