c201b74037760966a500e4d39b19f09afad69959
Joaovitorcoelho <48135760+Joaovitorcoelho@users.noreply.github.com>
2019-03-15 16:10:45 -0300

    Update exercicio9.php

--- exercicio9.php
+++ exercicio9.php
@@ -1,21 +1,17 @@
 <?php
-print "digite o tamanho da área a ser pintada(m²): ";
+print "digite o tamanho da área a ser pintada M²: ";
 $areaparapintar = fgets(STDIN);
 $folga = $areaparapintar+(($areaparapintar*10)/100);
 $qlata = ceil($folga/108);
 $precodalata = $qlata*80;
 print "precisará de $qlata latas de 18L. \n";
 print "Comprar as latas custará um total de $precodalata \n";
-print "\n";
 print "ou \n";
-print "\n";
 $Qgalao = ceil($folga/21.6);
 $Precogalao = $Qgalao*25;
 print "precisará de $Qgalao galãos de 3,6L. \n";
 print "Comprar os galões custará um total de $Precogalao. \n";
-print "\n";
 print "ou \n";
-print "\n";
 $Qgalao2 = $Qgalao;
 $Qlata2 = 0;
 if ($Qgalao2 >= 5) {
@@ -25,4 +21,3 @@ $Qgalao2 = ceil($Qgalao2%5);
 $totalapagar = ($Qgalao2*25)+($Qlata2*80);
 print "você precisará de $Qgalao2 galãos e $Qlata2 latas de tinta. \n";
 print "O total a pagar será de $totalapagar";
-
